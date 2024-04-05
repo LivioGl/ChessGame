63e644ed06938420c2ae9b9968f78cb30225573b
LivioGl <liviogalleano3@gmail.com>
2024-04-05 17:26:53 +0200

    Class Pieces added

---
+++ Source/Chess_Project/Private/Knight.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Knight.h"
+
+// Sets default values
+AKnight::AKnight()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AKnight::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKnight::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Chess_Project/Public/Queen.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ChessPiece.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Queen.generated.h"
+
+UCLASS()
+class CHESS_PROJECT_API AQueen : public AChessPiece
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AQueen();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
